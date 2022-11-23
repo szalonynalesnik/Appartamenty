e3d771d283879eb8136aee4a1a1f143b07a08f42
Anna Myszkier <anna.myszkier@gmail.com>
2022-11-23 01:03:28 +0100

    Divide login screen into composables

--- app/src/main/java/com/example/appartamenty/LoginActivity.kt
+++ app/src/main/java/com/example/appartamenty/LoginActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -44,7 +45,7 @@ class LoginActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginForm(applicationContext)
+                    LoginScreen(applicationContext)
                 }
             }
         }
@@ -53,115 +54,132 @@ class LoginActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginForm(context: Context) {
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    val image = painterResource(R.drawable.applogo)
-
+fun LoginScreen(context: Context) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(modifier = Modifier.padding(vertical = 16.dp)) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Image(
-                    painter = image,
-                    contentDescription = "logo",
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .size(50.dp)
-                        .padding(horizontal = 8.dp),
-                    colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.primary)
-                )
-                Text(
-                    text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.headlineLarge,
-                    color = MaterialTheme.colorScheme.primary,
-                    fontWeight = FontWeight.Light
-                )
-            }
-            Text(
-                text = "Welcome again!",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.outline,
-                modifier = Modifier
-                    .fillMaxWidth(),
-                textAlign = TextAlign.Center
-            )
-        }
+        LoginWelcome(context = context)
+        LoginForm(context = context)
+    }
+}
 
-        Column(
+@Composable
+fun LoginWelcome(context: Context) {
+    val image = painterResource(R.drawable.applogo)
+
+    Column(
+        modifier = Modifier
+            .padding(bottom = 24.dp),
+    ) {
+        Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp, horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = {
-                    Text(
-                        text = "E-mail address",
-                    )
-                },
-                placeholder = {
-                    Text(
-                        text = "Enter your e-mail address...",
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.outline
-                    )
-
-                },
-                leadingIcon = {
-                    Icon(
-                        Icons.Default.Email,
-                        contentDescription = "email"
-                    )
-                },
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = image,
+                contentDescription = "logo",
+                contentScale = ContentScale.Fit,
                 modifier = Modifier
-                    .fillMaxWidth(),
+                    .size(50.dp)
+                    .padding(horizontal = 8.dp),
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.primary)
             )
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = {
-                    Text(
-                        text = "Password"
-                    )
-                },
-                placeholder = {
-                    Text(
-                        text = "Enter your password...",
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.outline,
-                    )
-
-                },
-                leadingIcon = {
-                    Icon(
-                        Icons.Default.Lock,
-                        contentDescription = "lock"
-                    )
-                },
-                modifier = Modifier
-                    .fillMaxWidth(),
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Light,
+                modifier = Modifier,
             )
         }
+        Text(
+            text = "Nice to see you again!",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.outline,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginForm(context: Context) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = {
+                Text(
+                    text = "E-mail address",
+                )
+            },
+            placeholder = {
+                Text(
+                    text = "Enter your e-mail address...",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.outline
+                )
+
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Email,
+                    contentDescription = "email"
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = {
+                Text(
+                    text = "Password"
+                )
+            },
+            placeholder = {
+                Text(
+                    text = "Enter your password...",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.outline,
+                )
+
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Lock,
+                    contentDescription = "lock"
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+
+        )
         Button(
             onClick = {
                 logged(email, password, context)
             },
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 100.dp, end = 100.dp, top = 16.dp)
+                .padding(top = 24.dp),
         ) {
             Text(
                 modifier = Modifier
@@ -174,6 +192,11 @@ fun LoginForm(context: Context) {
         }
     }
 }
+
+//@Composable
+//fun LoginButton(email: String, password: String, context: Context) {
+//
+//}
 
 fun logged(email: String, password: String, context: Context) {
     if (email == "ania" && password == "1234") {
@@ -188,6 +211,6 @@ fun logged(email: String, password: String, context: Context) {
 fun DefaultPreview() {
     val context = LocalContext.current
     AppartamentyTheme {
-        LoginForm(context)
+        LoginScreen(context)
     }
 }
